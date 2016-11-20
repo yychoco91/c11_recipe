9f21bf6bd109f1fd3a9d92173e7ff14380ee5a53
Kevin Chau <kchau.jobs@gmail.com>
2016-11-19 19:04:03 -0800

    sample json setup

---
+++ database_test.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 11/19/16
+ * Time: 4:32 PM
+ */
+{
+    "criteria": {
+    "q": null,
+    "allowedIngredient": null,
+    "excludedIngredient": null
+  },
+  "matches": [
+    {
+        "imageUrlsBySize": {
+        "90": "https://lh3.googleusercontent.com/5y3EDiTt6VGRX5BizPvwK1C3LGDEa897vkBWHj4kq44OHBQMNlywdoDcP5PJCDUeym73zFS-U2q899xUMcUq_w=s90-c"
+      },
+      "sourceDisplayName": "Vodka and Biscuits",
+      "ingredients": [
+        "brussels sprouts",
+        "olive oil",
+        "salt",
+        "pepper",
+        "pomegranate",
+        "toasted pine nuts",
+        "grated parmesan cheese",
+        "lemon"
+    ],
+      "id": "Roasted-Brussels-Sprouts-with-Pomegranate-1920349",
+      "smallImageUrls": [
+        "https://lh3.googleusercontent.com/ndDOwxpCpgTis7wfEUPsmlognTRvUpWw7fzbB4hVI-nAQoO6cwlRCdbBRW0A10e4eHjMU6gtN2nDlq7txn44=s90"
+    ],
+      "recipeName": "Roasted Brussels Sprouts with Pomegranate",
+      "totalTimeInSeconds": 2700,
+      "attributes": {
+        "course": [
+            "Side Dishes"
+        ],
+        "holiday": [
+            "Sunday Lunch"
+        ]
+      },
+      "flavors": null,
+      "rating": 4
+    },
+    {
+        "imageUrlsBySize": {
+        "90": "https://lh3.googleusercontent.com/C6D-gRY098_W14hVFAyvijzCdQ1LJim37lkXe5zlcwuwnST1cQw8Emr5zKkrd1Lz3lN7pm3Tp2eyjm-yLWYA4yY=s90-c"
+      },
+      "sourceDisplayName": "Cookie Rookie",
+      "ingredients": [
+        "bacon",
+        "yellow onion",
+        "garlic",
+        "green beans",
+        "black pepper",
+        "kosher salt",
+        "red pepper flakes",
+        "water",
+        "bourbon whiskey",
+        "dark brown sugar"
+    ],
+      "id": "Skillet-Bourbon-Bacon-Green-Beans-1928194",
+      "smallImageUrls": [
+        "https://lh3.googleusercontent.com/QnqEeM-84d46B4oezuQHemULde558klfZAMsAQhtZ_H3PBITQ4Ojw8jnPivrsKAtSvdwAqTBwazIotEYunUbJA=s90"
+    ],
+      "recipeName": "Skillet Bourbon Bacon Green Beans",
+      "totalTimeInSeconds": 1200,
+      "attributes": {
+        "course": [
+            "Side Dishes"
+        ]
+      },
+      "flavors": {
+        "piquant": 0.16666666666666666,
+        "meaty": 0.16666666666666666,
+        "bitter": 0.16666666666666666,
+        "sweet": 0.5,
+        "sour": 0.5,
+        "salty": 0.8333333333333334
+      },
+      "rating": 4
+    }
+}
+
+?>
---
+++ json_test.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 11/19/16
+ * Time: 5:40 PM
+ */
+
+$output = [
+    'success'=>true,
+    'data'=>[
+        [
+            'recipe_name'=>"Roasted Brussels Sprouts with Pomegranate",
+            'ingredients'=>[
+                "brussels sprouts",
+                "olive oil",
+                "salt",
+                "pepper",
+                "pomegranate",
+                "toasted pine nuts",
+                "grated parmesan cheese",
+                "lemon"
+            ],
+            "totalTimeInSeconds"=> 2700,
+            "imageUrlsBySize"=> [
+    "90"=> "https://lh3.googleusercontent.com/5y3EDiTt6VGRX5BizPvwK1C3LGDEa897vkBWHj4kq44OHBQMNlywdoDcP5PJCDUeym73zFS-U2q899xUMcUq_w=s90-c"
+      ]
+
+        ],
+        [
+
+        ]
+    ]
+];
+
+?>
