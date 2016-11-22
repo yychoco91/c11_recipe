e1d79038a549efc3db006c239be7ae8d30c22bcc
Kevin Chau <kchau.jobs@gmail.com>
2016-11-22 12:07:43 -0800

    working on getting ingredients

--- db_prototype/read_file.php
+++ db_prototype/read_file.php
@@ -13,19 +13,33 @@
     <meta name="viewport" content="width=device-width">
     <title>API Get Ingredients</title>
     <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+    <script src="connect.php"></script>
 </head>
 <body>
 <button onclick="getRecipes();">Get Recipes</button>
 </body>
 <script>
     function getRecipes(){
+        console.log('Getting recipes');
         $.ajax({
-           datatype: 'json',
+            dataType: 'json',
             method: 'get',
-            url:'http://api.yummly.com/v1/api/recipes'
+            url:'http://api.yummly.com/v1/api/recipes?maxResult=20',
             headers: yummlyAccess,
             success: function(resp){
-
+                console.log('Success', resp);
+                var ingredientsList = [];
+                var recipesList = [];
+                var returnQuery = resp.matches;
+                console.log(resp.matches);
+                for(var i = 0; i < returnQuery.length; i++) {
+                    for(var j = 0; j < returnQuery[i].ingredients.length; j++) {
+                        if (ingredientsList.indexOf(returnQuery[i].ingredients[j]) === -1) {
+                            ingredientsList.push(returnQuery[i].ingredients[j]);
+                        }
+                    }
+                }
+                console.log('Ingredients List Complete: ', ingredientsList);
             }
         });
     }
