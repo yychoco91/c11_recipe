f3ecfa0b7416fee9adc50e59a4272ee98d6a9aa8
Kevin Chau <kchau.jobs@gmail.com>
2016-11-21 19:19:08 -0800

    populate-ingredients prototype

--- db_prototype/read_file.php
+++ db_prototype/read_file.php
@@ -5,4 +5,29 @@
  * Date: 11/20/16
  * Time: 3:31 PM
  */
-$myfile = fopen('recipe/Yummly_recipe');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>API Get Ingredients</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+</head>
+<body>
+<button onclick="getRecipes();">Get Recipes</button>
+</body>
+<script>
+    function getRecipes(){
+        $.ajax({
+           datatype: 'json',
+            method: 'get',
+            url:'http://api.yummly.com/v1/api/recipes'
+            headers: yummlyAccess,
+            success: function(resp){
+
+            }
+        });
+    }
+</script>
+</html>
