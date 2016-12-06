c0acbf1d1b850609ed98e642a8cc79b06d5f4ad0
Yrenia Yang <yrenia @ gmail.com>
2016-12-05 18:49:20 -0800

    Fixed database connection errors-Yrenia

--- db_prototype/get_recipes.php
+++ db_prototype/get_recipes.php
@@ -41,7 +41,7 @@ foreach ($userIngredients as $ingredient_id) {
                                )';
 }
 
-$query_temp = 'SELECT r.`recipe_ID`, r.`name`, r.`author`, r.`url`, r.`picture_url`, r.`instructions`, r.`cookTime`. ' . $query_scoring_part . ' AS match_count
+$query_temp = 'SELECT r.`recipe_ID`, r.`name`, r.`author`, r.`url`, r.`picture_url`, r.`instructions`, r.`cookTime`,' . $query_scoring_part . ' AS match_count
                FROM recipes r
                ORDER BY match_count DESC
                LIMIT 20
