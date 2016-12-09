395c581add5d49829abd454d4b3a3fd42c8b1b5e
Ever Garcia <uriel621@live.com>
2016-12-09 10:25:50 -0800

    ajax call on removing ingredient buttons

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ var getIngredients = function () {
 var getRecipe = function () {
     $.ajax({
 
+
         url: "./db_prototype/get_recipes.php",
         dataType: "json",
         method: "post",
@@ -71,7 +72,7 @@ var getRecipe = function () {
             ingredients: ingredientsID
         },
         success: function (response) {
-
+            getBackItems();   /*put function here so that it is called after ajax call for getRecipe - is completed*/
             console.log("data from get_recipes.php\n", response);
             clear();
             var authorName;
@@ -99,7 +100,7 @@ var getRecipe = function () {
                 });
                 var img = $("<img>", {
                     src: imgSrc,
-                    class: "showImage img-responsive",
+                    class: "showImage img-responsive cover",
                     width: "100%",
                     height: "286px",
                     'data-toggle': "modal",
@@ -155,11 +156,10 @@ var clear = function () {
     $("#stuff").empty()
 };
 /**
- * buttonsPushedToMainDisplay - Buttons On Ingredients Suggestion Pushed to Main Display
+ * buttonsPushedToMainDisplay - Buttons On NAV to Main Display
  */
 var buttonsPushedToMainDisplay = function () {
     $(".btn.btn-info.topIng").click(function () {
-
         var val = $(this).attr("value");
         var txt = $(this).text();
         txtArr.push(txt);
@@ -192,7 +192,9 @@ var removeIng = function () {
     ingredientsID.splice(indexS, 1);
 
     $(this).closest("button").remove();
-    console.log("Current Items in Fridge", ingredientsID)
+    console.log("Current Items in Fridge", ingredientsID);
+    getRecipe();
+
 };
 var addClickHandlerToRemovableIngredient = function (element) {
     element.on('click', removeIng);
@@ -392,3 +394,13 @@ var recipe_info_from_jsonphp_file = function () {
         }
     })
 };
+
+
+function getBackItems(){
+    console.log("jjj")
+    if(ingredientsID.length === 0){
+        clear()
+        recipe_info_from_jsonphp_file();
+        console.log("jjj2")
+    }
+}
