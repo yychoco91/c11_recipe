c445722f79f66fabc6e3c5b9cdbbf63ccad7b389
Kevin Chau <kchau.jobs@gmail.com>
2016-12-12 15:20:24 -0800

    added files to match ones on server

--- db_prototype/recipe/get_featured_recipe.php
+++ db_prototype/recipe/get_featured_recipe.php
@@ -60,6 +60,6 @@ if ($result = $conn->query($query_temp)) {
 if($output['success']) {
     $output_json = json_encode($output);
     $featuredRecipeFile = fopen('./featuredRecipeList.js', "w") or die("Unable to open file");
-    fwrite($featuredRecipeFile, $output_json);
+    fwrite($featuredRecipeFile, "var featureRecipesList = $output_json");
     fclose($featuredRecipeFile);
 }
