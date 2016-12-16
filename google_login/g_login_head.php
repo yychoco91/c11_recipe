f74db5b522daa196c6bb5ed56c2bd02d4031ddd4
root <root@ip-172-31-10-47.us-west-1.compute.internal>
2016-12-16 20:07:19 +0000

    changed http to https

--- google_login/g_login_head.php
+++ google_login/g_login_head.php
@@ -7,7 +7,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500" rel="stylesheet">
-<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!--jQuery UI-->
--- jef_braintree/braintree_head.php
+++ jef_braintree/braintree_head.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="braintreestyle.css">
     <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
     <script src="https://cdn.jsdelivr.net/lettering/0.7.0/jquery.lettering.min.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script src="jquery.printarea.js" type="text/JavaScript" language="javascript"></script>
     <script src="demo.js"></script>
 </head>
